ff87bab9d54b6d00a0fa5d8c86127846b9c9d7b9
Rupithkumar <133633120+Rupithkumar@users.noreply.github.com>
2024-08-30 15:53:12 +0530

    new update

--- Leet problem codes/palindrome number.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if x<0:
-            return False
-        else:
-           s=str(x)
-           return s==s[::-1]
--- Leet problem codes/plus one.py
+++
@@ -1,3 +0,0 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        return list(map(int,str(int(''.join(map(str,digits)))+1)))
